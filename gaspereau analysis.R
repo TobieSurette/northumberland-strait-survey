fc4c92bc0ec32e1863f1ee4bf8e77e93b4f2f0ea
Tobie Surette <tobie.surette@gmail.com>
2021-03-29 13:10:34 -0300

    Update gaspereau

--- gaspereau analysis.R
+++ gaspereau analysis.R
@@ -1,19 +1,102 @@
 library(mgcv)
+library(glmmTMB)
 
+# Load data:
 load("gaspereau 2007-2019.rdata")
 
-# Strategy:
-#
-#
-#
-# Because of the grid-based nature of the NS surveys spatial sampling design, a natural strategy for
-# handling spatial random effects are Conditional Auto-Regressive models over space and possibly time.
-#
-# To do:
-# - Convert station IDs to xy integer coordinates.
-# - Test simple CAR model
-# - Check for annual varying of depth relationships.
-# - Check if TMB can handle CAR models.
+aggregate(data$f, by = data["year"], mean)
+
+model <- glmmTMB(f ~ year, family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (year | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (year + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (depth + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(depth + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (length + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(length + 0 | group), family = poisson, data = data, verbose = TRUE)
+
+# model <- glmmTMB(f ~ (station + 0 |group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(year + 0 | group) + exp(position + 0 | group), family = poisson, data = data, verbose = TRUE)
+
+# Year x depth:
+model <- glmmTMB(f ~ ar1(year + 0 | group) + ar1(depth + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (depth + 0 | year), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (year + 0 | depth), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(depth + 0 | year), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(year + 0 | depth), family = poisson, data = data, verbose = TRUE)
+
+# Year x length:
+model <- glmmTMB(f ~ ar1(year + 0 | group) + ar1(length + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (length + 0 | year), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ (year + 0 | length), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(length + 0 | year), family = poisson, data = data, verbose = TRUE)
+
+# Quadratic length:
+model <- glmmTMB(f ~ (len|year) + len2 + ar1(length + 0 | year), family = poisson, data = data, verbose = TRUE)
+newdata <- data.frame(year = factor(2014, levels = levels(data$year)),
+                      len = 13:33,
+                      len2 = (13:33)^2,
+                      length = factor(13:33, levels = levels(data$length)))
+gbarplot(predict(model, newdata = newdata), 13:33)
+
+
+
+
+model <- glmmTMB(f ~ year + ar1(length + 0 | year), family = poisson, data = data, verbose = TRUE)
+newdata <- data.frame(year = factor(2013, levels = levels(data$year)),
+                      len = 13:33,
+                      len2 = (13:33)^2,
+                      length = factor(13:33, levels = levels(data$length)))
+gbarplot(exp(predict(model, newdata = newdata)), 13:33)
+
+model <- glmmTMB(f ~ (length + 0 | year), family = poisson, data = data, verbose = TRUE)
+
+image(2007:2019, 13:33, t(as.matrix(ranef(model)[[1]]$length)))
+
+# Length, depth and year:
+model <- glmmTMB(f ~ year + ar1(depth + 0 | group) + ar1(length + 0 | year), family = poisson, data = data, verbose = TRUE)
+newdata <- data.frame(year = factor(2009, levels = levels(data$year)),
+                      depth = sort(unique(data$depth))[6],
+                      group = factor(1),
+                      length = factor(13:33, levels = levels(data$length)))
+gbarplot(exp(predict(model, newdata = newdata)), 13:33)
+
+model <- glmmTMB(f ~ ar1(depth + 0 | group) + ar1(year + 0 | length), family = poisson, data = data, verbose = TRUE)
+
+# Position models:
+model <- glmmTMB(f ~ exp(position + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ exp(position + 0 | group), family = poisson, data = data, verbose = TRUE)
+model <- glmmTMB(f ~ ar1(depth + 0 | group) + ar1(length + 0 | year) + exp(position + 0 | group), family = poisson, data = data, verbose = TRUE)
+
+grid$group <- factor(1)
+grid$year <- factor(2009, levels(data$year))
+grid$length <- factor(25, levels(data$length))
+mu <- predict(model, newdata = grid)
+
+model <- glmmTMB(f ~ ar1(depth + 0 | group) + ar1(length + 0 | year) + exp(position + 0 | group), family = poisson, data = data, verbose = TRUE)
+
+# This seems to converge:
+formula <- f ~ ar1(depth + 0 | group) +      # AR(1) depth effects
+               ar1(length + 0 | year) +      # AR(1) length effects
+               exp(position + 0 | group) +   # Global spatial effect.
+               exp(position + 0 | year)      # Anuual spatial effects.
+model2 <- glmmTMB(formula, family = poisson, data = data, verbose = TRUE)
+
+# Next add gear effects, gear x length, maybe, tow distance offsets:
+# Station effects may be necessary to do comparative effects:
+# Model, local, annual, length-frequencies.
+
+predict(model, newdata = grid)
+
+# Figure out how to specify simple random effects:
+model <- glmmTMB(f ~ length, family = poisson, data = r, verbose = TRUE)
+model <- glmmTMB(f ~ (length | group), family = poisson, data = r, verbose = TRUE)
+model <- glmmTMB(f ~ (length | year), family = poisson, data = r, verbose = TRUE)
+model <- glmmTMB(f ~ (year|group) + (length | year), family = poisson, data = r, verbose = TRUE)
+
+# Depth effect
+# Depth*year effect
+# length effect
+# length * year effect
 
 
 # Fit various models:
@@ -23,14 +106,48 @@ model[[2]] <- gamm(f ~ 1 + s(xkm, ykm) + offset(off), random = list(year = ~1, l
 model[[3]] <- gamm(f ~ 1 + s(xkm, ykm) + offset(off), random = list(year = ~1, length = ~ 1), family = poisson, data = data)
 model[[4]] <- gamm(f ~ 1 + offset(off), random = list(year = ~1, length = ~ 1, station = ~1), family = poisson, data = data)
 
-# To do:
-# Predict over spatial grid:
-# Map spatial grid for a single year.
-# Map spatial grid over all years.
-# Predict length-frequencies by year with errors.
-# Estimate annual abundance indices.
 
-# Stop here!
+library(mvtnorm)
+library(rockchalk)
+simGroup <- function(g, n=6, rho=0.7) {
+    x <- mvrnorm(mu = rep(0,n),
+             Sigma = rho ^ as.matrix(dist(1:n)) )   ## Simulate the process
+    y <- x + rnorm(n)                               ## Add measurement noise
+    times <- factor(1:n)
+    group <- factor(rep(g,n))
+    data.frame(y, times, group)
+}
+dat1 <- do.call("rbind", lapply(1:1000, simGroup) )
+
+(fit.ar1 <- glmmTMB(y ~ ar1(times + 0 | group), data=dat1))
+
+dat1$times <- numFactor(dat1$times)
+levels(dat1$times)
+
+fit.mat <- glmmTMB(y ~ mat(times + 0 | group), data=dat1, dispformula=~0)
+fit.exp <- glmmTMB(y ~ exp(times + 0 | group), data=dat1)
+
+
+d <- data.frame(z = as.vector(volcano),
+                x = as.vector(row(volcano)),
+                y = as.vector(col(volcano)))
+
+set.seed(1)
+d$z <- d$z + rnorm(length(volcano), sd=15)
+d <- d[sample(nrow(d), 100), ]
+
+volcano.data <- array(NA, dim(volcano))
+volcano.data[cbind(d$x, d$y)] <- d$z
+image(volcano.data, main="Spatial data", useRaster=TRUE)
+
+d$pos <- numFactor(d$x, d$y)
+d$group <- factor(rep(1, nrow(d)))
+
+f <- glmmTMB(z ~ 1 + exp(pos + 0 | group), data=d)
+
+x <- sample(1:2, 10, replace=TRUE)
+y <- sample(1:2, 10, replace=TRUE)
+(pos <- numFactor(x,y))
 
 # Draw map of samples:
 map.new(xlim = c(-65.25, -61.75), ylim = c(45.5, 47.25))
--- prepare data.R
+++ prepare data.R
@@ -17,10 +17,10 @@ x$longitude <- -dmm2deg(lon(x))
 x$latitude  <- dmm2deg(lat(x))
 x$depth     <- depth(x$longitude, x$latitude)
 x <- x[x$depth > 0, ]
-x$station   <- station(x, method  = "latlong")
-tmp        <- deg2km(x$longitude, x$latitude)
-names(tmp) <- c("xkm", "ykm")
-x      <- cbind(x, tmp)
+x$station   <- deblank(station(x, method  = "latlong"))
+tmp         <- deg2km(x$longitude, x$latitude)
+names(tmp)  <- c("xkm", "ykm")
+x           <- cbind(x, tmp)
 fvars       <- names(x)[gsub("[0-9]", "", names(x)) == ""]
 
 # Prepare interpolation grid:
@@ -30,6 +30,28 @@ tmp        <- deg2km(grid$longitude, grid$latitude)
 names(tmp) <- c("xkm", "ykm")
 grid       <- cbind(grid, tmp)
 grid$depth <- depth(grid$longitude, grid$latitude)
+grid$station <- deblank(grid$station)
+grid$depth    <- round(depth(grid$longitude, grid$latitude)/5)*5 # Round depth by 5m bins.
+grid <- grid[grid$depth > 0, ]
+grid$depth <- numFactor(grid$depth)
+
+# Determine integer grid coordinates:
+dx <- mean(diff(sort(unique(grid$longitude))))
+dy <- mean(diff(sort(unique(grid$latitude))))
+fun <- function(x, y){
+   v <- NA * x
+   for (i in 1:length(x)) v[i] <- which.min(abs(x[i] - y))
+   return(v)
+}
+grid$x <- fun(grid$longitude, seq(min(grid$longitude), max(grid$longitude), dx))
+grid$y <- fun(grid$latitude, seq(min(grid$latitude), max(grid$latitude), dy))
+grid$position <- numFactor(grid$x, grid$y)
+
+ix <- match(x$station, grid$station.number)
+x$x <- grid$x[ix]
+x$y <- grid$y[ix]
+x$position <- grid$position[ix]
+x <- x[!is.na(x$x) & !is.na(x$y), ]  # Remove problem grid coordinates.
 
 # Prepare data for analysis:
 x           <- x[, setdiff(names(x), as.character(c(0:12, 34:50)))] # Remove small and over-large fish.
@@ -41,10 +63,15 @@ data <- data.frame(f         = as.vector(as.matrix(x[fvars])),
                    gear      = as.factor(rep(x$gear.type, each = length(fvars))),
                    xkm       = rep(x$xkm, each = length(fvars)),
                    ykm       = rep(x$ykm, each = length(fvars)),
-                   depth     = rep(x$depth, each = length(fvars)),
-                   log.depth = rep(log(x$depth), each = length(fvars)),
+                   x         = rep(x$x, each = length(fvars)),
+                   y         = rep(x$y, each = length(fvars)),
+                   position  = rep(x$position, each = length(fvars)),
                    station   = as.factor(rep(x$station, each = length(fvars))),
                    off       = as.numeric(rep(log(x$distance / 0.625), each = length(fvars))))
 
-save(data, file = "gaspereau 2007-2019.rdata")
+data          <- data[!is.na(data$x) & !is.na(data$y), ]       # Problems with grid coordinates.
+data$offset   <- log(data$distance / 0.625)                    # Tow distance offset term.
+data$group    <- factor(rep(1, nrow(data)))                    # Dummy variable to treat global effects.
+data$depth    <- grid$depth[match(data$station, grid$station)] # Get depth from survey grid.
 
+save(data, grid, file = "gaspereau 2007-2019.rdata")
